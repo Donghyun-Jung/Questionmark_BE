bbce3283ffb18bc08c51fab3e601f7dcd0d7f97b
Yeonming <2019110914@dongguk.edu>
2024-05-17 21:33:50 +0900

    alarm

--- src/main/java/com/example/duel/alarm/AlarmRequest.java
+++ src/main/java/com/example/duel/alarm/AlarmRequest.java
@@ -1,5 +1,14 @@
-package com.example.duel.alarm;
+package com.example.duel .alarm;
+
+import java.util.List;
 
 public class AlarmRequest {
-    
+
+    public record ReadAlarmDTO(
+            List<AlarmDTO> alarms
+    ) {
+        public record AlarmDTO(
+                Long id
+        ) { }
+    }
 }
