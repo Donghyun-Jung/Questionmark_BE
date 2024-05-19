8bc887699cfa9bd6b0f24f9e1b576085c6bdcbc5
Yeonming <2019110914@dongguk.edu>
2024-05-19 22:15:05 +0900

    image , comment , roadmap

--- src/main/java/com/example/duel/comment/CommentResponse.java
+++ src/main/java/com/example/duel/comment/CommentResponse.java
@@ -1,5 +1,14 @@
 package com.example.duel.comment;
 
+
 public class CommentResponse {
-    
+
+    public record CreateCommentDTO(
+            Long id
+    ) {
+        public CreateCommentDTO(Comment comment) {
+            this(comment.getId());
+        }
+    }
+
 }
--- src/main/java/com/example/duel/roadmap/Category.java
+++ src/main/java/com/example/duel/roadmap/Category.java
@@ -1,5 +1,27 @@
 package com.example.duel.roadmap;
 
-public class Category {
-    
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Category {
+    CATEGORY_INDIVIDUAL("individual"),
+    CATEGORY_GROUP("group"),
+    CATEGORY_DUEL("duel");
+
+    private String value;
+
+    public String getString(Category category) {
+        return this.value;
+    }
+
+    public static Category getCategory(String category) {
+        if (category.equals("individual"))
+            return CATEGORY_INDIVIDUAL;
+        else if (category.equals("group"))
+            return CATEGORY_GROUP;
+        else
+            return CATEGORY_DUEL;
+    }
 }
